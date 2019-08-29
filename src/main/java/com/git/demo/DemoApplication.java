8eb55d9461be35ce34f126150d18d6ce2190dd20
JokerYanjiangshan <https://github.com/JokerYanjiangshan/springlcn.git>
2019-08-29 21:44:56 +0800

    本地git

--- src/main/java/com/git/demo/DemoApplication.java
+++ src/main/java/com/git/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     //mygitmasterbrach
-    //我改的 分支
+    //我改的 分支 我的是呵呵找事
     //我在本地分支修改 提交github上分支
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
