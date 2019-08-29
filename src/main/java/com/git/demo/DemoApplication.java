2b5ea507feff0b39b36281758bba0e79c18e6b21
JokerYanjiangshan <47970895+JokerYanjiangshan@users.noreply.github.com>
2019-08-29 21:29:01 +0800

    Update DemoApplication.java

--- src/main/java/com/git/demo/DemoApplication.java
+++ src/main/java/com/git/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
     //mygitmasterbrach
-
+    //我改的 分支
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
